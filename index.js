bd947e79ab1902af8921360ff0903df09113cb1c
TheNinza <niks.a3198@gmail.com>
2022-01-18 21:22:15 +0530

    modified middleware

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { API_ACCESS_KEY } = require("./configs/environments");
 
 /**
  * Express configuration
@@ -12,28 +11,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization; // Bearer <token>
-    if (authHeader.startsWith("Bearer ")) {
-      const token = authHeader.split(" ")[1];
-      if (token !== API_ACCESS_KEY) {
-        throw new Error("Invalid API key");
-      } else {
-        next();
-      }
-    } else {
-      throw new Error("Invalid API key");
-    }
-  } catch (error) {
-    res.status(401).json({
-      message: "Invalid API key",
-    });
-  }
-});
 
 // endpoints
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
--- routes/getSpotifyData.js
+++ routes/getSpotifyData.js
@@ -1,8 +1,31 @@
 const { getCurrentOrLastSong } = require("../controllers/getSpotifyData");
+const { API_ACCESS_KEY } = require("../configs/environments");
 
 const router = require("express").Router();
 
 // router endpoints
-router.get("/getCurrentOrLastSong", getCurrentOrLastSong);
+router.get(
+  "/getCurrentOrLastSong",
+  (req, res, next) => {
+    try {
+      const authHeader = req.headers.authorization; // Bearer <token>
+      if (authHeader.startsWith("Bearer ")) {
+        const token = authHeader.split(" ")[1];
+        if (token !== API_ACCESS_KEY) {
+          throw new Error("Invalid API key");
+        } else {
+          next();
+        }
+      } else {
+        throw new Error("Invalid API key");
+      }
+    } catch (error) {
+      res.status(401).json({
+        message: "Invalid API key",
+      });
+    }
+  },
+  getCurrentOrLastSong
+);
 
 module.exports = router;
